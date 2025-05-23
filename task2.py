de806c23ece6e14d46a344aa9af8cd2dde635c53
Viktoriia Matiushko <vikamatuchko772@gmail.com>
2025-05-23 14:33:42 +0300

    --

--- task2.py
+++ task2.py
@@ -2,7 +2,7 @@ from collections import deque
 import re
 
 
-def is_palindrome():
+def is_palindrome() -> bool:
     str = input("Enter your string: ")
 
     str_clean = re.findall(r"\w", str.lower())
@@ -17,5 +17,6 @@ def is_palindrome():
 
     print("String is palindrome")
     return True
+
 
 is_palindrome()
